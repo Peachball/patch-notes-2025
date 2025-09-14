e8704af442d3cf61e46517c37a0aea812db133af
Peachball <chenyexu@gmail.com>
2025-09-14 16:18:34 -0700

    Add a weird noisy background

--- scenes/components/gravity_component.gd
+++ scenes/components/gravity_component.gd
@@ -8,11 +8,9 @@ extends Node
 var is_falling: bool = false
 
 func handle_gravity(body: CharacterBody2D, delta: float) -> void:
-		if not body.is_on_floor():
-			body.velocity.y = minf(
-				body.velocity.y + gravity * delta,
-				terminal_velocity
-			)
-		is_falling = body.velocity.y > 0 and not body.is_on_floor()
-		
-		
+	if not body.is_on_floor():
+		body.velocity.y = minf(
+			body.velocity.y + gravity * delta,
+			terminal_velocity
+		)
+	is_falling = body.velocity.y > 0 and not body.is_on_floor()
