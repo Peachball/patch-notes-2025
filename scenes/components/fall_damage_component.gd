01ac432791bc68b5569f603a5d44f78ed49317cf
Max Haak <haakm7@gmail.com>
2025-09-14 18:41:15 -0700

    add damage thresholds

--- scenes/components/fall_damage_component.gd
+++ scenes/components/fall_damage_component.gd
@@ -3,8 +3,8 @@ extends Node
 
 @export_subgroup("Settings")
 @export var safe_fall_time: float = 1.0
-@export var damage_fall_time: float = 2.0
-@export var fall_damage_amount: int = 1
+@export var damage_thresholds: Array[float] = [1.5, 3.0, 4.5]
+@export var damage_amounts: Array[int] = [1, 2, 3]
 
 var fall_time: float = 0.0
 var was_falling: bool = false
@@ -21,9 +21,17 @@ func handle_fall_damage(body: CharacterBody2D, gravity_component: GravityCompone
 		_reset_fall_tracking()
 
 func _check_fall_damage(health_component: HealthComponent) -> void:
-	if fall_time > damage_fall_time:
-		health_component.take_damage(fall_damage_amount)
-		fall_damage_dealt.emit(fall_damage_amount)
+	if fall_time <= safe_fall_time:
+		return
+	
+	var damage_to_deal = 0
+	for i in range(damage_thresholds.size()):
+		if fall_time > damage_thresholds[i]:
+			damage_to_deal = damage_amounts[i]
+	
+	if damage_to_deal > 0:
+		health_component.take_damage(damage_to_deal)
+		fall_damage_dealt.emit(damage_to_deal) 
 
 func _reset_fall_tracking() -> void:
 	fall_time = 0.0
